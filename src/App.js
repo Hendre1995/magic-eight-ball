c5af05db289f3df6ad4899a3af6e19b6c2660ef2
Hendre1995 <hendrebreedt1995@gmail.com>
2022-03-03 16:39:28 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"
 
-function App() {
+const answers = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes - definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again"
+];
+
+const App=()=> {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const getAnswer = (e) => {
+    e.preventDefault();
+    if (!question) {
+      return;
+    }
+    const index = Math.floor(Math.random() * answers.length);
+    setAnswer(answers[index]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <style>
+        {`
+          .circle {
+            background:white;
+            border: 1px solid black;
+            border-radius: 50%;
+            width: 500px;
+            height: 500px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+          `}
+      </style>
+      <form onSubmit={getAnswer}>
+        <div>
+          <label>question</label>
+          <input
+          className="input"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+        </div>
+        <button 
+        className=""
+        type="submit">get answer</button>
+      </form>
+      <div className="circle">
+        <p>{answer}</p>
+      </div>
     </div>
   );
 }
